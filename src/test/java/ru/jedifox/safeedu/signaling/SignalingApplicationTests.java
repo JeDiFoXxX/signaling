ef222f6eaf7186abe0a0a87e9b8764b3e44db062
JeDiFoXxX <serjik1993991@gmail.com>
2026-05-16 16:31:36 +0300

    Create project

---
+++ src/test/java/ru/jedifox/safeedu/signaling/SignalingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.jedifox.safeedu.signaling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignalingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
